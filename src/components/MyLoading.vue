b8c9557dbbe5fb14a95d4409d2922225ef52c5cf
YongSangYou <dydtkd113@gmail.com>
2022-05-30 22:21:46 +0900

    fetch: loading

---
+++ src/components/MyLoading.vue
@@ -0,0 +1,65 @@
+<template>
+  <div 
+    v-if="isShow"
+    class="modal" >
+    <div class="modal__inner">
+      <svg
+        viewBox="0 0 38 38"
+        xmlns="http://www.w3.org/2000/svg"
+        width="50"
+        height="50">
+        <g fill="none" fillRule="evenodd">
+          <g transform="translate(1 1)">
+            <path
+              d="M36 18c0-9.94-8.06-18-18-18"
+              stroke="#0444A1"
+              strokeWidth="2">
+              <animateTransform
+                attributeName="transform"
+                type="rotate"
+                from="0 18 18"
+                to="360 18 18"
+                dur="0.9s"
+                repeatCount="indefinite" />
+            </path>
+            <circle fill={color} cx="36" cy="18" r="1">
+              <animateTransform
+                attributeName="transform"
+                type="rotate"
+                from="0 18 18"
+                to="360 18 18"
+                dur="0.9s"
+                repeatCount="indefinite" />
+            </circle>
+          </g>
+        </g>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    isShow: {
+      type: Boolean,
+      default: false
+    }
+  }  
+}
+
+</script>
+<style scoped>
+  .modal {
+    background-color: rgba(0, 0, 0, .2);
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    z-index: 9;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+</style>

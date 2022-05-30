b97efc1d82244b85649afce254467b7455b4c0a2
YongSangYou <dydtkd113@gmail.com>
2022-05-30 22:20:23 +0900

    fetch: ErrorText

---
+++ src/components/ErrorText.vue
@@ -0,0 +1,24 @@
+<template>
+  <span>올바른 {{ text }}를 입력해주세요.</span>  
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+<style scoped>
+  span {
+    display: block;
+    color: #F3494A;
+    font-size: 13px;
+  }
+  .active {
+    visibility: hidden;
+  }
+</style>

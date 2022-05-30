6f5c24e8991b403db45a0d0266a344ea69f24b2d
YongSangYou <dydtkd113@gmail.com>
2022-05-30 22:21:20 +0900

    fetch: Button

---
+++ src/components/MyButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <button>
+    <slot />
+  </button>
+</template>
+
+<style scoped>
+  button {
+    padding: 16px 0;
+    background-color: #0444A1;
+    border: none;
+    border-radius: 5px;
+    outline: none;
+    color: #FFF;
+    font-weight: 600;
+    cursor: pointer;
+  }
+  button:disabled {
+    background-color: #DEDFE2;
+    cursor: default;
+  }
+</style>

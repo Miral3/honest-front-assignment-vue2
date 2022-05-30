1666f7dd72343ea4d92dd141b565718d7814d51c
YongSangYou <dydtkd113@gmail.com>
2022-05-30 22:19:44 +0900

    fetch: Input

---
+++ src/components/MyInput.vue
@@ -0,0 +1,27 @@
+<template>
+  <input
+  :style="{width: typeof size === 'number' 
+  ? `${size}px` : size}" 
+  @input="$emit('update', $event)" />
+</template>
+
+<script>
+export default {
+  props: {
+    size: {
+      type: [String, Number],
+      default: 80
+    }
+  }
+}
+</script>
+
+<style scoped>
+  input {
+    padding: 10px 10px;
+    border: 1px solid #E2E3E6;
+    border-radius: 6px;
+    outline: none;
+    box-sizing: border-box;
+  }
+</style>

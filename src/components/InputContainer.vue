722557a01c97dbc703c754bb6eaa28fb195d65bd
YongSangYou <dydtkd113@gmail.com>
2022-05-30 22:19:56 +0900

    fetch: InputContainer

---
+++ src/components/InputContainer.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+  div {
+    position: relative;
+    margin-top: 8px;
+    margin-bottom: 3px;
+    color: #E2E3E6;
+  }
+</style>

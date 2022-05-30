a2ad438494a92262e07922d72c68b6572d13d4fa
YongSangYou <dydtkd113@gmail.com>
2022-05-30 22:18:39 +0900

    fetch: CardFrom

---
+++ src/components/CardForm.vue
@@ -0,0 +1,32 @@
+<template>
+  <form @submit.prevent="onSubmit">
+    <slot/>
+  </form>
+</template>
+
+<script>
+export default {
+    props: {
+      onSubmit: {
+        type: Function,
+        default: function() {
+          return;
+        }
+      }
+    }
+}
+</script>
+
+<style scoped>
+form {
+  display: flex;
+  flex-direction: column;
+  width: 616px;
+  height: 624px;
+  padding: 64px 120px;
+  border-radius: 16px;
+  background-color: #FFF;
+  box-shadow: 0 0 1px rgba(0, 0, 0, 0.2);
+  box-sizing: border-box;
+}
+</style>

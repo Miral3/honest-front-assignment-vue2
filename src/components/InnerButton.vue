d81a5c8cb8152b8eb2d50ece9a978d40a83c587f
YongSangYou <dydtkd113@gmail.com>
2022-05-30 22:22:03 +0900

    fetch: innerButton

---
+++ src/components/InnerButton.vue
@@ -0,0 +1,32 @@
+<template>
+  <button @click="onClick">
+    <slot />
+  </button>  
+</template>
+
+<script>
+export default {
+    props: {
+      onClick: {
+        type: Function,
+        default: function() {
+          return;
+        }
+      }
+    }
+}
+</script>
+
+<style scoped>
+  button {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    padding-right: 10px;
+    border: none;
+    background: none;
+    cursor: pointer;
+    color: #2E85FF;
+  }
+</style>

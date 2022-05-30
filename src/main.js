7e09f3eb6dcbeb9fb2f62b23ca1a27a24df2a894
YongSangYou <dydtkd113@gmail.com>
2022-05-30 22:21:01 +0900

    fetch: main component use plugins

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@ import Vue from 'vue'
 import App from './App.vue'
 // router
 import router from '@/router'
+//plugin
+import validate from './plugins/validate'
+import request from './plugins/request'
 
 Vue.config.productionTip = false
+
+Vue.use(validate);
+Vue.use(request);
 
 new Vue({
   render: (h) => h(App),
